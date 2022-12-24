13da1a1c52ec232392cf456170d30384295e147f
Divin Dass <116546151+WebX-Divin@users.noreply.github.com>
2022-12-24 14:10:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = ""
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
