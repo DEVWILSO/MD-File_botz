db17a189082c359cb84797dda095d4a516d81dd6
Divin Dass <116546151+WebX-Divin@users.noreply.github.com>
2022-12-24 13:24:00 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/WebX-Divin/File-Sharing-Bot'>Click here</a>\n○ Channel : @WebXBots\n○ Support Group : @Web_X_Support \n○ Credits : @CodeXBotz</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
